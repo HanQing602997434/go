76569111fc051ece84a11d3ef8d90700b233f6eb
hq <602997434@qq.com>
2021-10-27 02:38:00 -0700

    循环—while和do while

--- loop.go
+++ loop.go
@@ -38,4 +38,31 @@
 				for index, val := range str {
 				}
 				使用的是字符的方式遍历
+
+	while循环控制
+		Go中没有while
+		for {
+			if i > 10 {
+				break
+			}
+			fmt.Println("hello world!", i)
+			i++
+		}
+
+	do while循环控制
+		Go中没有do while
+		for {
+			fmt.Println("Hello World!", i)
+			i++
+			if i > 10 {
+				break
+			}
+		}
+		
+	多重循环控制
+		1）将一个循环放在另一个循环体内，就形成了嵌套循环。在外边的for称为外层循环	
+		在里面的for循环称为内层循环。【建议一般使用两层，最多不要超过3层】
+		2）实质上，嵌套循环就是把内层循环当成外层循环的循环体。当只有内层循环的循环
+		条件为false时，才会完全跳出内层循环，才可结束外层的档次循环，开始下一次循环。
+		3）外层循环次数为m次，内层为n次，则内层循环体实际上需要执行m*n=mn次 
 */
