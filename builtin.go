f724cb1db2f9afe207914cf5ce98794ed00c95af
hq <602997434@qq.com>
2021-11-22 02:11:56 -0800

    内置函数

---
+++ builtin.go
@@ -0,0 +1,12 @@
+
+// 内置函数
+/*
+	说明：
+		Golang设计者为了编程方便，提供了一些函数，这些函数可以直接使用，我们成为Go的内置函数。
+			1)len：用来求长度，比如string、array、slice、map、channel。
+
+			2)new：用来分配内存，主要用来分配值类型，比如int、float32、struct...返回的是指针。
+				num := new(int)
+
+			3)make：用来分配内存，主要用来分配引用类型，比如channel、map、slice。
+*/
