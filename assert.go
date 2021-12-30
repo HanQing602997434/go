0cd3502266e4377781c8d547e4bb105f2c341d56
hq <602997434@qq.com>
2021-12-29 21:36:08 -0800

    类型断言

--- assert.go
+++ assert.go
@@ -33,4 +33,51 @@
 		在进行类型断言时，如果类型不匹配，就会报panic，因此进行类型断言时，要确保原来的空
 		接口指向的就是断言的类型。
 		在进行类型断言时，带上检测机制，如果成功就ok，否则也不要报panic
+
+	类型断言的最佳实践
+		type Student struct {
+			name string
+			age int
+		}
+
+		func TypeJudge(items... interface{}) {
+			for index, x := range items {
+				switch x.(type) {
+				case bool:
+					fmt.Printf("第%v个参数是 bool 类型，值是%v\n", index, x)
+				case float32:
+					fmt.Printf("第%v个参数是 float32 类型，值是%v\n", index, x)
+				case float64:
+					fmt.Printf("第%v个参数是 float64 类型，值是%v\n", index, x)
+				case int, int32, int64:
+					fmt.Printf("第%v个参数是 整数 类型，值是%v\n", index, x)
+				case string:
+					fmt.Printf("第%v个参数是 string 类型，值是%v\n", index, x)
+				case Student:
+					fmt.Printf("第%v个参数是 Student 类型，值是%v\n", index, x)
+				case *Student:
+					fmt.Printf("第%v个参数是 *Student 类型，值是%v\n", index, x)
+				default:
+					fmt.Printf("第%v个参数的类型不确定，值是%v\n", index, x)
+				}
+			}
+		}
+
+		func main() {
+			var n1 float32 = 1.1
+			var n2 float64 = 2.3
+			var n3 int32 = 30
+			var name string = "tom"
+			address := "北京"
+			n4 := 300
+			
+			stu := Student{
+				name : "tom",
+				age : 18,
+			}
+
+			ptr := &stu
+
+			TypeJudge(n1, n2, n3, name, address, n4, stu, ptr)
+		}
 */
